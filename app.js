6092f4dfc53b686e65d300c3d52e6be9513ab672
¨Luis <luchoav6@gmail.com>
2022-05-20 12:40:11 -0300

    Subida

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 function saludar (){
     let nombreusuario= prompt("Ingresar Nombre");
-    alert("Hola " + nombreusuario);
+    let apellidousuario= prompt("Ingresar apellido");
+    alert("Hola " + nombreusuario + " "+apellidousuario+","+" bienvenido");
+   
 }
+
+const bienvenida = [
+    saludar()
+]
+
+let texto = document.createElement("h3")
+texto.innerHTML="<h3>Hola ${nombreusuario} ${apellidousuario}!! Bienvenido a tu formulario</h3>";
+document.body.append (texto);
+
+
 
