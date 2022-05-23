33f59bdb8a547e01555ee87f55c6d362c6dda417
¨Luis <luchoav6@gmail.com>
2022-05-23 11:00:01 -0300

    Subida

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ function saludar (){
     let nombreusuario= prompt("Ingresar Nombre");
     let apellidousuario= prompt("Ingresar apellido");
     alert("Hola " + nombreusuario + " "+apellidousuario+","+" bienvenido");
-   
+
 }
+function text(){ 
+let textodinamico= document.getElementById('dinamico');
+textodinamico.innerHTML= '<h3> Hola {nombreusuario} {apellidousuario}!! bienvenido </h3>'
+}
+
+text()
 
 const bienvenida = [
     saludar()
 ]
 
-let texto = document.createElement("h3")
-texto.innerHTML="<h3>Hola ${nombreusuario} ${apellidousuario}!! Bienvenido a tu formulario</h3>";
-document.body.append (texto);
+//let texto = document.createElement("h3")
+//texto.innerHTML="<h3>Hola ${nombreusuario} ${apellidousuario}!! Bienvenido a tu formulario</h3>";
+//document.body.append (texto);
 
 
 
